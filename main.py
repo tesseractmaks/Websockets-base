fbd253c1aaaceed8b072e67fd9862e00ff079440
vlaskinmac <tesseractmaks@gmail.com>
2023-09-22 10:47:00 +0800

    Save messages history to file

--- main.py
+++ main.py
@@ -1,4 +1,19 @@
 import asyncio
+import datetime
+
+from pathlib import Path
+
+import aiofiles
+
+OUT_PATH = (Path(__file__).parent / 'chat.log').absolute()
+
+
+async def write_to_disk(data):
+    file_path = OUT_PATH
+    time_now = datetime.datetime.now().strftime("%d.%m.%Y %H:%M:%S")
+    async with aiofiles.open(file_path, mode='a') as f:
+        # for i in data:
+        await f.write(f"[{time_now}] {data.decode()!r}\n")
 
 
 async def get_messages():
@@ -7,6 +22,12 @@ async def get_messages():
     )
     while True:
         data = await reader.read(300)
+        await write_to_disk(data)
         print(f'Received: {data.decode()!r}')
+    writer.close()
+    await writer.wait_closed()
 
 asyncio.run(get_messages())
+
+
+# print(x)
