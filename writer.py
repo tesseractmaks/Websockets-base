40dc99ab36c3460185a58f09c0ab0b26ec0e0114
tesseract <tesseractmaks@gmail.com>
2024-03-23 20:55:03 +0800

    Escape control symbols

--- writer.py
+++ writer.py
@@ -1,8 +1,9 @@
+import argparse
 import asyncio
 import json
 import aiofiles
 from pathlib import Path
-
+from passlib.context import CryptContext
 from config import sender_log
 
 accounts = {
@@ -11,9 +12,17 @@ accounts = {
 ACCOUNTS_PATH = (Path(__file__).parent / 'accounts.json').absolute()
 
 
+def decode_utf8(data):
+    return data.decode("utf-8", "ignore")
+
+
+def encode_utf8(data):
+    return data.encode("utf-8", "ignore")
+
+
 async def write_accounts(account, file_path=ACCOUNTS_PATH):
     async with aiofiles.open(file_path, mode='a', encoding='utf-8') as file:
-        await asyncio.to_thread(json.dump(account, file, ensure_ascii=False, indent=4))
+        await file.write(json.dumps(account, ensure_ascii=False, indent=4))
 
 
 async def read_accounts(account, file_path=ACCOUNTS_PATH):
@@ -23,39 +32,7 @@ async def read_accounts(account, file_path=ACCOUNTS_PATH):
             return account if account in accounts else False
 
 
-async def register(HOST, PORT, token):
-    sender_log.debug("Enter your personal hash for create new account.")
-    reader, writer = await asyncio.open_connection(
-        HOST, PORT
-    )
-    accounts = {
-        "account_hash": token
-    }
-    writer.close()
-    await writer.wait_closed()
-    await write_accounts(accounts)
-
-    await authorise(HOST, PORT, token)
-
-
-async def authorise(HOST, PORT, token):
-    reader, writer = await asyncio.open_connection(
-        HOST, PORT
-    )
-    account = await read_accounts(token)
-
-    if account:
-        sender_log.debug("Success! You are enter in account!")
-        return account["account_hash"]
-    else:
-        sender_log.debug("Неизвестный токен. Проверьте его или зарегистрируйте заново.")
-        await register(HOST, PORT, token)
-
-    await register(HOST, PORT, token)
-    await authorise(HOST, PORT, token)
-
-async def submit_message(HOST, PORT, message):
-
+async def register(HOST, PORT, parser):
     reader, writer = await asyncio.open_connection(
         HOST, PORT
     )
@@ -63,18 +40,98 @@ async def submit_message(HOST, PORT, message):
     data = await reader.readline()
     sender_log.debug(f'{data.decode()!r}')
 
-    # writer.write(token.encode())
-    # sender_log.debug(token)
+    writer.write("\n".encode())
+    await writer.drain()
+
+    data2 = await reader.readline()
+    sender_log.debug(f'{data2.decode()!r}')  # 'Enter preferred nickname below:\n'
+
+    writer.write(parser.reg.encode())
+    sender_log.debug(f"{parser.reg} --")  # DEBUG:writer:nik --
+    await writer.drain()
+
+    writer.write("\n".encode())
+    await writer.drain()
+
+    data3 = await reader.readline()
+    sender_log.debug(f'{data3.decode()!r}')
+
+    # await write_accounts(json.loads(data3.decode()))
+
+    writer.write("\n".encode())
+    await writer.drain()
+
+    # find_nickname = await read_accounts(parser.nickname)
+    #
+    # if find_nickname:
+    #     sender_log.debug("Username already exist.")
+    #     writer.close()
+    #     await writer.wait_closed()
+    #     return
+    #
+    # pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+    # hash_code = pwd_context.hash(find_nickname.decode())
+    #
+    # accounts = {
+    #     "nickname": find_nickname.decode(),
+    #     "account_hash": hash_code
+    # }
+    # sender_log.debug(accounts)
+    #
+    # writer.write(accounts.encode())
+    # sender_log.debug(f"{parser.nickname} --")  # DEBUG:writer:nik --
     # await writer.drain()
 
-    # token = await reader.readline()
-    # if not json.loads(token):
-    #     sender_log.debug("Неизвестный токен. Проверьте его или зарегистрируйте заново.")
-    #     await writer.drain()
-    # else:
-    #     sender_log.debug(f'{token.decode()!r}')
 
-    writer.write(message.encode())
+    writer.close()
+    await writer.wait_closed()
+    # await write_accounts(accounts)
+
+    # await authorise(HOST, PORT, token)
+
+
+async def authorise(HOST, PORT, parser):
+    reader, writer = await asyncio.open_connection(
+        HOST, PORT
+    )
+
+    data = await reader.readline()
+    sender_log.debug(f'{data.decode()!r}')
+
+    # writer.write("\n".encode())
+    # await writer.drain()
+    #
+    # data2 = await reader.readline()
+    # sender_log.debug(f'{data2.decode()!r}')  # 'Enter preferred nickname below:\n'
+
+    writer.write(parser.token.encode())
+    sender_log.debug(f"{parser.token} --")  # DEBUG:writer:nik --
+    await writer.drain()
+
+    writer.write("\n".encode())
+    await writer.drain()
+
+    data3 = await reader.readline()
+    sender_log.debug(f'{data3.decode()!r}')
+
+    message = "!!! -My message -!!!"
+
+    await submit_message(reader, writer, message)
+
+
+async def submit_message(reader, writer, message):
+
+    writer.write("\n".encode())
+    await writer.drain()
+
+    data = await reader.readline()
+    writer.write(data)
+
+    writer.write("\n".encode())
+    await writer.drain()
+    await asyncio.sleep(0.05)
+
+    writer.write(encode_utf8(f"{message.strip()}\n"))
     sender_log.debug(message)
     await writer.drain()
 
@@ -84,9 +141,28 @@ async def submit_message(HOST, PORT, message):
     writer.close()
     await writer.wait_closed()
 
-message = "!!! -My message -!!!\n"
 
-send_host = "minechat.dvmn.org"
-send_port = 5050
+def argparser():
 
-asyncio.run(submit_message(send_host, send_port, message))
+    parser = argparse.ArgumentParser(description="Chat client")
+
+    parser.add_argument("-ht", "--host", type=str, default="minechat.dvmn.org", help="Enter host")
+    parser.add_argument("-p", "--port", type=int, default=5050, help="Enter port")
+    parser.add_argument("-t", "--token", type=str,  help="Enter hash token")
+    parser.add_argument("-r", "--reg", type=str,  help="Enter nickname for registration")
+    return parser.parse_args()
+
+
+async def main():
+    parser = argparser()
+    host = parser.host
+    port = parser.port
+
+    if parser.reg:
+        await register(host, port, parser)
+
+    if parser.token:
+        await authorise(host, port, parser)
+
+
+asyncio.run(main())
